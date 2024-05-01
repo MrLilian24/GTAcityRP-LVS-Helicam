c83ac58116e0a66bc77fcaed846b1eded28528d3
Rémi LAVERGNE <remi.lavergne@etu.uca.fr>
2024-05-01 23:21:45 +0200

    ⬆️ Upload des fichiers

---
+++ lua/autorun/sv_helicam.lua
@@ -0,0 +1,22 @@
+-- sv_helicam.lua - Server side
+-- Created by Rémi.L (https://steamcommunity.com/id/lilian24)
+
+util.AddNetworkString("RequestVehiclePlate")
+util.AddNetworkString("SendLicensePlate")
+
+local PLATE = LL_PLATES_SYSTEM or {}
+
+net.Receive("RequestVehiclePlate", function(len, ply)
+    local player = net.ReadEntity()
+    local vehicle = net.ReadEntity()
+
+    if IsValid(player) and IsValid(vehicle) then
+        PLATE:GetLicensePlate(player, vehicle, function(err, plate)
+            if not err then
+                net.Start("SendLicensePlate")
+                net.WriteString(plate)
+                net.Send(player)
+            end
+        end)
+    end
+end)
